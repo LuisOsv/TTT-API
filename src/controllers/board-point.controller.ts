e9447c9c3a558041f07679198e86fc56f7daf54a
LuisOsv <lsosvaldo@gmail.com>
2019-02-03 23:00:36 -0400

    Adding logic to determine winner

--- src/controllers/board-point.controller.ts
+++ src/controllers/board-point.controller.ts
@@ -20,4 +20,43 @@ export class BoardPointController {
     return await this.boardRepo.points(id).create(point);
   }
 
+  @post('/board/{id}/point')
+  async createPointAndVerify(@param.path.number('id') id: number,
+    @requestBody() point: Point) {
+    // verify horizontal winner
+    // 1. get all stored points for same symbol and same coordinate Y
+    var points = await this.boardRepo.points(id).find({
+      where: {
+        symbol: point.symbol,
+        y: point.y
+      }
+    });
+    points.push(point);
+    points.sort((a, b) => a.x - b.x);
+
+    points.forEach(element => {
+      console.log(element.x + " - " + element.y);
+    });
+
+    // 2. verify if all points are consecutive x coordinates
+    /* var isConsecutive = true;
+    for (var index = 0; index < points.length - 1; index++) {
+      if (!(points[index].x < points[index + 1].x)) {
+        isConsecutive = false;
+        break;
+      }
+    }
+
+    if (isConsecutive) {
+      console.log('Winner');
+    } */
+
+    // verify vertical winner
+    // verify diagonal winner
+
+    return await this.boardRepo.points(id).create(point);
+  }
+
+
+
 }
